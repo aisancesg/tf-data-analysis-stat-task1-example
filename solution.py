fd46e49508417a56e70f1b14c2aa087646a155bf
aisancesg <129902469+aisancesg@users.noreply.github.com>
2023-04-07 21:36:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1407630156 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return 0.1*(x.mean()+6) # Ваш ответ
